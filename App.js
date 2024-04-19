a1507e3b8e05eb9306f2714182a5ffbd3d228b05
Admin <Admin@DESKTOP-5UHIEA6>
2024-04-19 18:55:31 +0700

    Lab1

--- App.js
+++ App.js
@@ -1,15 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+import Bai1 from './src/Bai1';
+import Bai2 from './src/Bai2';
+import Bai3 from './src/Bai3';
+import Bai4 from './src/Bai4';
+import Bai5 from './src/Bai5';
+import Bai6 from './src/Bai6';
+import Bai7 from './src/Bai7';
+import Bai8 from './src/Bai8';
+import Phan2 from './src/Phan2';
+const  App=()=>{
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Phan2/>
+
   );
 }
-
+export default App;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
